a31e8e38a7980841fc06d19ac4f7f710676fbc7d
duc1892006-png <duc1892006@gmail.com>
2025-11-06 14:15:44 +0700

    Add files via upload

---
+++ Bai3/bai3_1.py
@@ -0,0 +1,9 @@
+def sum(a, b):
+    print("sum = " + str(a + b))
+
+# Tính tổng 2 số 4 và 5
+sum(4, 5)
+
+# Tính tổng 2 số 3 và 7
+sum(3, 7)
+ 
---
+++ Bai3/bai3_2.py
@@ -0,0 +1,5 @@
+def sum(a, b):
+    return a + b
+
+c = sum(4,5 )
+print("Tổng của a  và b  = " + str(c))
